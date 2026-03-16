980da1ac467d0e0f82247bd7fa2427f8798097e2
AbdurlhmanBadwy <meshmasre@gmail.com>
2026-03-16 13:16:37 +0200

    Variables

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+</head>
+<body>
+    <?php
+        $names = "Dark Matter";
+    ?>
+    <h1>
+
+        <!-- <?php
+            $greeting ="Hello";
+            echo "$greeting Everybody!";
+        ?> -->
+
+        You have read <?php echo $names;?>
+    </h1>
+</body>
+</html>
