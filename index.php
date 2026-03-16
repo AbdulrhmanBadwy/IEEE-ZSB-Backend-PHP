c0476f20b0006cc18ea14d28065f9eada457e7fc
AbdurlhmanBadwy <meshmasre@gmail.com>
2026-03-16 16:45:02 +0200

    Phase 2 Page links

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
 <?php
-$name = "Abdulrhman Badwy";
-$cost = 15;
 
-$business = [
-    'name' => 'Laracasts',
-    'cost' => 15,
-    'categories' => ['testing', 'php', 'javascript']
-];
-
-$business['name'];
-
-// foreach ($business['categories'] as $category){
-//     echo $category ."<br>";
-// }
-
-function register($user){
-    // Create the user recored in the db 
-    // log them in . 
-    // send a welcom email. 
-    // readirect to their new dashboard . 
-
-}
 
 require "inedex.view.php"; 
 
