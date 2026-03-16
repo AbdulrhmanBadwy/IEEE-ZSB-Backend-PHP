8384eb7571dba87ac2935bd8ba104c6f9560c301
AbdurlhmanBadwy <meshmasre@gmail.com>
2026-03-16 14:23:35 +0200

    functions and filter

--- index.php
+++ index.php
@@ -1,36 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo</title>
 </head>
+
 <body>
     <h1>Recommended Books </h1>
     <?php
-        $books = [
-            [
-                "name"=> "Do Androids Dream of Electric SheeP",
-                "author"=>"Philip K. Dick",
-                "purchaseUrl"=>"http:// emxmaple.com",
-            ],
-            [
-                "name"=>'Project Hali Mary',
-                "author"=>'Andy Weir',
-                "purchaseUrl"=>'http://example.com',
-            ]
-            
-        ];
+    $books = [
+        [
+            "name" => "Do Androids Dream of Electric SheeP",
+            "author" => "Philip K. Dick",
+            'releaseYear' => 1968,
+
+            "purchaseUrl" => "http://emxmaple.com",
+        ],
+        [
+            "name" => 'Project Hali Mary',
+            "author" => 'Andy Weir',
+            'releaseYear' => 2011,
+
+            "purchaseUrl" => 'http://example.com',
+        ],
+        [
+            'name' => 'The Martian',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2011,
+            'purchaseUrl' => 'http://example.com',
+        ]
+
+    ];
+    function filterByAuthor($author) 
+    {
+        global $books;
+        $filteredBooks = [];
+
+        foreach ($books as $book) { 
+            if($book['author' ] == $author){
+                array_push($filteredBooks, $book);
+            }
+        }
+        return $filteredBooks;
+
+    }
+
+
     ?>
-    
+
     <ul>
-        <?php foreach ($books as $book): ?>
-            <li>  
-                <a href="<?= $book['purchaseUrl']?>">
-                    <?= $book['name']?> 
+        <?php foreach (filterByAuthor('Philip K. Dick') as $book): ?>
+
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name']; ?> (<?= $book['releaseYear'] ?> ) - <?= $book['author']?>
                 </a>
             </li>
-        <?php endforeach ?> 
+        <?php endforeach ?>
     </ul>
 </body>
+
 </html>
