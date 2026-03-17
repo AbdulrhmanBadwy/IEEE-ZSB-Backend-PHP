d18e4d336779e6b16503619e430262293aa411bb
AbdurlhmanBadwy <meshmasre@gmail.com>
2026-03-17 13:59:31 +0200

    PDO first step connecting to database and fetch all data as ASSOC_array

--- index.php
+++ index.php
@@ -2,4 +2,17 @@
 
 require 'functions.php';
 
-require 'router.php';
+// require 'router.php';
+
+// connect to our mysql database 
+$dsn = 'mysql:host=localhost;port= 3306;dbname=myapp;user=root;password=223374;charset=utf8mb4';
+$pdo = new PDO($dsn );
+
+$statement = $pdo-> prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>". $post['title'] . "</li>"; 
+}
