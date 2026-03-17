eb91be29a3f3784241dbb687075491a04fd21f20
AbdurlhmanBadwy <meshmasre@gmail.com>
2026-03-17 06:43:59 +0200

    make a php routing

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -11,10 +11,10 @@
                         <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
                         <a href="/" aria-current="page" class="<?= urlIs('/') ? "bg-gray-900 text-white" : "text-gray-300";
                         ?>  hover:700  px-3 py-2 text-sm font-medium text-white">Home</a>
-                        <a href="/about.php"
-                            class="<?= $_SERVER['REQUEST_URI'] == '/about.php' ? "bg-gray-900 text-white" : "text-gray-300";
+                        <a href="/about"
+                            class="<?= $_SERVER['REQUEST_URI'] == '/about' ? "bg-gray-900 text-white" : "text-gray-300";
                             ?> hover:bg-gray-700 px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                        <a href="/contact.php" class="<?= $_SERVER['REQUEST_URI'] == '/contact.php' ? "bg-gray-900 text-white" : "text-gray-300";
+                        <a href="/contact" class="<?= $_SERVER['REQUEST_URI'] == '/contact' ? "bg-gray-900 text-white" : "text-gray-300";
                         ?> text-gray-300 hover:bg-gray-700 hover: text-white px-3 py-2 text-sm
                             font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
 
