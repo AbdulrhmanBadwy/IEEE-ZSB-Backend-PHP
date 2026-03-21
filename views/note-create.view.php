bc2c3c5e470ab3b756854b35ac660d3d87ce19c7
AbdurlhmanBadwy <meshmasre@gmail.com>
2026-03-21 14:09:25 +0200

    Intro to Form Validation

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -6,7 +6,7 @@
 
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 text-white font-bold">
-        <form method="post" >
+        <form method="post">
             <div class="space-y-12">
                 <div class="border-b border-white/10 pb-12">
 
@@ -16,16 +16,22 @@
                             <label for="body" class="block text-sm/6 font-medium text-white">Body</label>
                             <div class="mt-2">
                                 <textarea id="body" name="body" rows="3"
-                                    class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6 " placeholder="Here's an idea for a note...  "></textarea>
-                            </div>
-                            <p class="mt-3 text-sm/6 text-gray-400">Write a few sentences about yourself.</p>
-                        </div>
-            </div>
+                                    class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                                    placeholder="Here's an idea for a note..."><?= $_POST['body'] ??'' ?></textarea>
+                                <?php if (isset($errors['body'])): ?>
+                                    <p class="text-red-500 text-xs mt-2">
+                                        <?= $errors['body'] ?>
+                                    </p>
+                                <?php endif ?>
 
-            <div class="mt-6 flex items-center justify-end gap-x-6">
-                <button type="submit"
-                    class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Save</button>
-            </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="mt-6 flex items-center justify-end gap-x-6">
+                        <button type="submit"
+                            class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Save</button>
+                    </div>
         </form>
 
     </div>
