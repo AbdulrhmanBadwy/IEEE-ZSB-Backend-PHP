625cceb64b19cdd9cc2d4a5bbd190a248c9bec3a
AbdurlhmanBadwy <meshmasre@gmail.com>
2026-03-21 13:35:07 +0200

    Always Escape Untrusted Input

--- views/note.view.php
+++ views/note.view.php
@@ -7,9 +7,10 @@
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 text-white font-bold">
         <p class=" text-blue-500 underline mb-6">
-            <a href="/notes">Go Back...  </a>
+            <a href="/notes">Go Back... </a>
         </p>
-            <p><?= $note['body'] ?></p>
+        <p> <?= htmlspecialchars($note['body']) ?>
+        </p>
     </div>
 </main>
 
