941b3213a7212997123cf2f67fab8688304b307a
AbdurlhmanBadwy <meshmasre@gmail.com>
2026-03-23 13:02:34 +0200

    Resourceful Naming Conventions

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -15,4 +15,4 @@ $note = $db->query('SELECT * from notes where   id = :id  ' , [
 
     authorize($note['user_id'] == $currentUserId);
 
-require("views/note.view.php");
+require("views/notes/show.view.php");
