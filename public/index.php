51f77371259720425471ea00050c76a654319849
AbdurlhmanBadwy <meshmasre@gmail.com>
2026-03-27 17:45:28 +0200

    Build a Better Router

--- public/index.php
+++ public/index.php
@@ -14,4 +14,10 @@ spl_autoload_register(function ($class) {
     }
 });
 
-require base_path('core/router.php');
+$router = new \core\Router();
+$routes = require base_path("routes.php");
+$url = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$method = $_POST['_method']?? $_SERVER['REQUEST_METHOD'];
+
+$router->route($url , $method);
