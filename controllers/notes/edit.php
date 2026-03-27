e2b1c76b2c159321dab21087bbb34a3af8fd254f
AbdurlhmanBadwy <meshmasre@gmail.com>
2026-03-27 21:41:17 +0200

    Updating with PATCH Requests

---
+++ controllers/notes/edit.php
@@ -0,0 +1,24 @@
+<?php
+
+use core\App;
+use core\Database;
+
+
+$db = App::resolve(Database::class);
+
+$currentUserId = 1;
+
+
+$note = $db->query('SELECT * from notes where   id = :id  ', [
+    'id' => $_GET["id"]
+
+])->findOrFail();
+
+authorize($note['user_id'] == $currentUserId);
+
+view("notes/edit.view.php", [
+    "heading" => 'Edit Note ',
+    'errors' => [],
+    'note' => $note
+]);
+
