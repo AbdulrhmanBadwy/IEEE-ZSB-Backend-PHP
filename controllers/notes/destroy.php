475c7707ceec1ee5f10d58c71ebe057735094a27
AbdurlhmanBadwy <meshmasre@gmail.com>
2026-03-27 19:04:23 +0200

    one request one view

--- controllers/notes/destroy.php
+++ controllers/notes/destroy.php
@@ -9,15 +9,16 @@ $db = new Database($config['database']);
 
 $currentUserId = 1;
 
-
 $note = $db->query('SELECT * from notes where   id = :id  ', [
-    'id' => $_GET["id"]
+    'id' =>$_POST["id"]
 
 ])->findOrFail();
 
 authorize($note['user_id'] == $currentUserId);
+$db->query("delete from notes where id = :id", [
 
-require view("notes/show.view.php", [
-    "heading" => 'Note',
-    'note' => $note,
+    'id' => $_GET['id']
 ]);
+
+header('location: /notes');
+exit();
